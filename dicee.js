e310b2e8358412775c2e9d803ce622bf80d8d726
Jack Conneely <jack@conneely.org>
2025-07-23 12:13:08 -0500

    test commit

--- dicee.js
+++ dicee.js
@@ -1,1 +1,6 @@
 // JavaScript file for Dicee game
+// test to see if this works
+
+document.querySelector(".img1").setAttribute("src", "images/dice6.png");
+document.querySelector(".img2").setAttribute("src", "images/dice6.png");
+
