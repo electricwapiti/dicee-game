6d6ffd3512cb9537e8d0d716249bab2175d0e6d7
Jack Conneely <jack@conneely.org>
2025-07-23 12:41:18 -0500

    finish game

--- dicee.js
+++
@@ -1,6 +0,0 @@
-// JavaScript file for Dicee game
-// test to see if this works
-
-document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-document.querySelector(".img2").setAttribute("src", "images/dice6.png");
-
